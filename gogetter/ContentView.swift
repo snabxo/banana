1e87f942ae9d9427026a3a718eced6c6d6a3d386
ghost <belikesnab@gmail.com>
2025-11-09 19:59:47 -0500

    Initial Commit

---
+++ gogetter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gogetter
+//
+//  Created by Samuel Brown on 11/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gogetter/gogetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  gogetterApp.swift
+//  gogetter
+//
+//  Created by Samuel Brown on 11/9/25.
+//
+
+import SwiftUI
+
+@main
+struct gogetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
